b34ae12c73fd1fe371179bc39b09497278541115
vewz <79582228+vewz@users.noreply.github.com>
2023-04-30 04:35:38 +0900

    Update mainLoop.mjs

--- docs/mainLoop.mjs
+++ docs/mainLoop.mjs
@@ -4,20 +4,32 @@ import { clearCanvas } from "./canvas.mjs";
 
 const { requestAnimationFrame } = globalThis;
 
+let isDebugging = false;
+Reflect.defineProperty(globalThis, "DAMMAKU_DEBUG", {
+  __proto__: null,
+  get: () => isDebugging,
+  set: v => isDebugging = !!v
+});
+const arg = Object.freeze({
+  __proto__: null,
+  hit() {
+    if (isDebugging) return;
+    console.error("Hit!");
+    resetPlayerPos();
+    return true;
+  },
+  grazed() {
+    console.info("Graze!");
+  }
+});
+
+
+
 export const startMainLoop = () => void function mainLoop() {
   clearCanvas();
   movePlayer();
   drawPlayer();
-  moveAndDrawShots({
-    hit() {
-      console.error("Hit!");
-      resetPlayerPos();
-      if (typeof DAMMAKU_DEBUG !== "undefined") return true;
-    },
-    grazed() {
-      console.info("Graze!");
-    }
-  });
+  moveAndDrawShots(arg);
   drawCollision();
   requestAnimationFrame(mainLoop);
 }();
